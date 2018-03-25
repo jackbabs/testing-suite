36601ed72aa33be1c8de791b6920034d7739217a
jackbaber <thejackbabs@gmail.com>
2018-03-25 12:48:07 +0100

    global setup

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,12 @@
-
+import jsdom from 'jsdom'
+import _$ from 'jquery'
 
 // set up testing environment to run like a browser in the command line 
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.window = global.document.defaultView
+const $ = _$(global.window)
 
-
-
-// build 'renderComponent helper that should render a given react class 
+// build 'renderComponent' helper that should render a given react class 
 
 
 
