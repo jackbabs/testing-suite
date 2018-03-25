80dd2896bd7529de43f670303069c2608f26defd
jackbaber <thejackbabs@gmail.com>
2018-03-25 12:36:34 +0100

    initial commit

---
+++ test/test_helper.js
@@ -0,0 +1,16 @@
+
+
+// set up testing environment to run like a browser in the command line 
+
+
+
+// build 'renderComponent helper that should render a given react class 
+
+
+
+// build helper for simulating events 
+
+
+
+
+// set up chai-jquery
